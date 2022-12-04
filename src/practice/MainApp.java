deaf18fa44e87c980329a6776a856017ceb2777a
Yevhen <shershen.yevhen@gmai>
2022-12-04 12:23:32 +0100

    Создание массива и его перебор

--- src/practice/MainApp.java
+++ src/practice/MainApp.java
@@ -8,5 +8,14 @@ man.callSex();
 Woman woman = new Woman("Ewa", "Krencicka", 30);
 woman.callPersonInfo();
 woman.callSex();
+//создаем массив с людьми
+Person [] persons = {
+        new Man("Alex", "Maximil",61),
+        new Woman("Ewa", "Krencicka", 30)
+};
+//перебираем каждый элемент в массиве и вызываем функцию callPersonInfo();
+for (Person el: persons){
+    el.callPersonInfo();
+}
     }
 }
