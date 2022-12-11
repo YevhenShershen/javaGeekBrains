b6c4e0b370300e527e01b0117bd1d42199ef1610
Yevhen <shershen.yevhen@gmai>
2022-12-11 18:03:23 +0100

    Szyfrator - GADERYPOLUKI (egzamin)

--- src/egzamins/Szyfrator.java
+++ src/egzamins/Szyfrator.java
@@ -2,8 +2,8 @@ package egzamins;
 
 public class Szyfrator {
     String word;
-    char[][] arrayEncrypt = {{'H', 'A'}, {'D', 'E'}, {'R', 'Y'}, {'P', 'O'}, {'L', 'U'}, {'K', 'I'}};
-    String encryptedWord;
+    String szyfr = "GADERYPOLUKI";
+    String encryptedWord = "";
 
     public Szyfrator(String word) {
         this.word = word;
@@ -11,17 +11,19 @@ public class Szyfrator {
 
     public void callEncryptedWord() {
         word = word.toUpperCase();
-        for (int i = 0; i <= word.length() - 1; i++) {
-            for (int y = 0; y <= arrayEncrypt.length - 1; y++) {
-                System.out.println(y);
-                if (word.charAt(i) == arrayEncrypt[y][0]) {
-                    encryptedWord += arrayEncrypt[y][1];
+        for (int a = 0; a < word.length(); a++) {
+            for (int b = 0; b < szyfr.length(); b++) {
 
-                } else if (word.charAt(i) == arrayEncrypt[y][1]) {
-                    encryptedWord += arrayEncrypt[y][0];
-                } else {
-                    encryptedWord += word.charAt(i);
+                if (word.charAt(a) == szyfr.charAt(b)) {
+                    if (b % 2 == 0) {
+                        encryptedWord += szyfr.charAt(b + 1);
+                    } else {
+                        encryptedWord += szyfr.charAt(b - 1);
+                    }
                 }
+            }
+            if (encryptedWord.length() - a == 0) {
+                encryptedWord += word.charAt(a);
             }
         }
         System.out.println(encryptedWord);
