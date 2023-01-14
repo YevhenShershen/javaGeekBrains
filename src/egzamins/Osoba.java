be9c5ec52368398b8cd2e1811b082f5461e729a9
Yevhen <shershen.yevhen@gmai>
2023-01-14 10:01:26 +0100

    некоторые изминения

--- src/egzamins/Osoba.java
+++ src/egzamins/Osoba.java
@@ -1,5 +1,9 @@
 package egzamins;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 //выделаем класс на который будем писать тесты и нажимаем alt+ENTER
 public class Osoba {
     private String imie;
@@ -31,5 +35,23 @@ public class Osoba {
         } else {
             System.out.println("Cześć " + name + ", mam na imie " + countObject);
         }
+    }
+    @Test
+    public void testEgzamin(){
+        //Wyświetlenie komunikatu <licznik>
+        System.out.println("Liczba zarejestrowanych osób to " + this.countObject);
+        //Utworzenie obiektu za pomocą konstruktora bezparametrowego
+        Osoba osoba1 = new Osoba();
+        osoba1.personInfo("Jan");
+        //Utworzenie obiektu za pomocą konstruktora z dwoma parametrami.
+        Osoba osoba2 = new Osoba(123, "Alex");
+        osoba2.personInfo("Jan");
+        //Utworzenie obiektu za pomocą konstruktora kopiującego
+        Osoba osoba3 = new Osoba(osoba2);
+        assertEquals(5, osoba3.countObject);
+        //Wywołanie metody do wypisania imienia z parametrem wejściowym równym „Jan”
+        osoba3.personInfo("Jan");
+        //Ponowne wyświetlenie komunikatu <licznik>
+        System.out.println("Liczba zarejestrowanych osób to " + this.countObject);
     }
 }
